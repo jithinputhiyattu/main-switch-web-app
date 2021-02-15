efc61e3c6d038b51d4eeb6a04170ecf41e9fc4bd
DHWANI <anushamuttoly@gmaiol.com>
2021-02-16 00:08:41 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import MainNavBar from "./components/main-nav-bar/MainNavBar";
 import { BrowserRouter, Route, Redirect} from "react-router-dom";
 import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
 import "./App.css";
 import { Component } from "react";
-import { Col, Row } from "react-bootstrap";
-import HomeImage from "./assets/img/homepage.jpg"
+
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <MainNavBar />
-        <Row>
-          <Col>
-          <h1>Main Switch</h1>
-          </Col>
-          <Col>
-          <img className = 'img-cont'  src = {HomeImage}></img>
-          </Col>
-        </Row>
+        < MainNavBar/>
+        <Home/>
         <Footer />
       </BrowserRouter>
     );
@@ -26,3 +19,4 @@ class App extends Component {
 }
 
 export default App;
+
