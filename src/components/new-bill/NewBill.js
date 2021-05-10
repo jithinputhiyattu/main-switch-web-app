a0d281280e39ae084c6f3bca430f18c808527aac
DHWANI <anushamuttoly@gmaiol.com>
2021-05-10 11:57:09 +0530

    dropdown

--- src/components/new-bill/NewBill.js
+++ src/components/new-bill/NewBill.js
@@ -1,38 +1,59 @@
 import { Component } from "react";
-import { Table } from "react-bootstrap";
+import { Table, Row, Col, Button, Container , Dropdown} from "react-bootstrap";
+import { MyDropDown } from "../drop-down/DropDown";
+
 
 class NewBill extends Component {
   render() {
     return (
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Item</th>
-            <th>Quantiy</th>
-            <th>MRP</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
+      <Row>
+        <Col>
+          <MyDropDown/>
+          <h3>Item</h3>
+        </Col>
+
+        <Col>
+          <h3>Unit</h3>
+        </Col>
+
+        <Col>
+          <h3>Price per unit</h3>
+        </Col>
+
+        <Col>
+          <Button className="add">Add</Button>
+        </Col>
+
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>SI No.</th>
+              <th>Item</th>
+              <th>unit</th>
+              <th>Price Per Unit</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Row>
     );
   }
 }
