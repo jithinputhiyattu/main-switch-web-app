377f6400474e356d4d15acd1111f0ce1c79c262d
DHWANI <anushamuttoly@gmaiol.com>
2021-05-28 10:48:25 +0530

    home_page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,10 +6,13 @@ import "./style.scss";
 class Footer extends PureComponent {
   render() {
     return (
-      <div class="block space-x-4 ...">
-        <div class="inline-block ...">Quick Links</div>
-        <div class="inline-block ...">About Us</div>
-        <div class="inline-block ...">Contact Us</div>
+      <div class="mx-auto p-1 sm:w-1/2">
+        <div class="text-xl font-semibold text-blue-500">Get In Touch!</div>
+        <div class="block space-x-4 content-center">
+          <div class="inline-block ...">Quick Links</div>
+          <div class="inline-block ...">About Us</div>
+          <div class="inline-block ...">Contact Us</div>
+        </div>
       </div>
     );
   }
--- src/components/main-nav-bar/MainNavBar.js
+++ src/components/main-nav-bar/MainNavBar.js
@@ -7,24 +7,40 @@ import "./style.scss";
 class MainNavBar extends PureComponent {
   render() {
     return (
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Navbar.Brand>
-            <Link to="/home">
-              <img className="logo" src={logo}></img>
-            </Link>
-          </Navbar.Brand>
-          <Nav className="mr-auto">
-            <Link to="/home" className="item">Home</Link>
-            <Link to="/features" className="item">Features</Link>
-            <Link to="/new-bill" className="item">New Bill</Link>
-            <Link to="/login" className="button">
-              <Button className="button">Login</Button>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <nav className="relative flex flex-wrap items-center justify-between bg-red-500 mb-3 bg-blue-300">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              href="#home"
+            >
+              HOME
+            </a>
+          </div>
+          <div>
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="text-lg leading-lg text-white opacity-75"></i>
+                  <span className="ml-2">Wiring Bill</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="text-lg leading-lg text-white opacity-75"></i>
+                  <span className="ml-2">Plumbing Bill</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
